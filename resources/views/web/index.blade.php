3baa83c4021d53f907386bd95d0aac7bf56426aa
Gabriel Galindo <gabrielgalindorodriguez@gmail.com>
2020-11-19 18:39:43 -0500

    primer commit

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -4,6 +4,8 @@
 	<div class="container-fluid">
 		<div class="row col-12">
 			<h1> Página en construcción </h1>
+			<br>
+			<p> Estaremos disponibles lo más pronto posible </p>
 		</div>
 	</div>
 

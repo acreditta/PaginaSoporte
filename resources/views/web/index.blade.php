9e91a37bf65cc158da1bc0c6b6518b458687616f
Paola Becerra <paonata_marquez@outlook.com>
2020-11-30 14:45:24 -0500

    run project

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,9 +1,27 @@
 @extends('layout.home')
 
+<?php
+function myfunction($v)
+{
+if ($v==="Dog")
+  {
+  return "Fido";
+  }
+return $v;
+}
+
+$a=array("Horse","Dog","Cat");
+print_r(array_map("myfunction",$a));
+?>
+
 @section('content')
 	<div class="container-fluid">
 		<div class="row col-12">
-			<h1> Página en construcción </h1>
+			<h1> Hola! ¿Cómo podemos ayudarte? </h1>
+		</div>
+		<div>
+		<div>Acreditados</div>
+		<div>Administradores</div>
 		</div>
 	</div>
 
